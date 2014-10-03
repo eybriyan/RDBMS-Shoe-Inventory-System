cabccca95a9ad984db266c92417db4e168bd806c
JakeyPayat <jakeymores@gmail.com>
2014-10-04 02:13:29 +0800

    Table And Buttons
    Table and BUTTon designs
    Hompage.php 60%

--- Homepage.php
+++ Homepage.php
@@ -1,5 +1,195 @@
 <html>
 <head>
+	<style>
+.TableID {
+	margin:0px;padding:0px;
+	width:100%;
+	border:1px solid #ffffff;
+	
+	-moz-border-radius-bottomleft:3px;
+	-webkit-border-bottom-left-radius:3px;
+	border-bottom-left-radius:3px;
+	
+	-moz-border-radius-bottomright:3px;
+	-webkit-border-bottom-right-radius:3px;
+	border-bottom-right-radius:3px;
+	
+	-moz-border-radius-topright:3px;
+	-webkit-border-top-right-radius:3px;
+	border-top-right-radius:3px;
+	
+	-moz-border-radius-topleft:3px;
+	-webkit-border-top-left-radius:3px;
+	border-top-left-radius:3px;
+}.TableID table{
+    border-collapse: collapse;
+        border-spacing: 0;
+	width:100%;
+	height:20%;
+	margin:0px;padding:0px;
+}.TableID tr:last-child td:last-child {
+	-moz-border-radius-bottomright:3px;
+	-webkit-border-bottom-right-radius:3px;
+	border-bottom-right-radius:3px;
+}
+.TableID table tr:first-child td:first-child {
+	-moz-border-radius-topleft:3px;
+	-webkit-border-top-left-radius:3px;
+	border-top-left-radius:3px;
+}
+.TableID table tr:first-child td:last-child {
+	-moz-border-radius-topright:3px;
+	-webkit-border-top-right-radius:3px;
+	border-top-right-radius:3px;
+}.TableID tr:last-child td:first-child{
+	-moz-border-radius-bottomleft:3px;
+	-webkit-border-bottom-left-radius:3px;
+	border-bottom-left-radius:3px;
+}.TableID tr:hover td{
+	background-color:#e5e5e5;
+		
+
+}
+.TableID td{
+	vertical-align:middle;
+		background:-o-linear-gradient(bottom, #ffffff 5%, #e5e5e5 100%);	background:-webkit-gradient( linear, left top, left bottom, color-stop(0.05, #ffffff), color-stop(1, #e5e5e5) ); 
+	background:-moz-linear-gradient( center top, #ffffff 5%, #e5e5e5 100% );
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr="#ffffff", endColorstr="#e5e5e5");	background: -o-linear-gradient(top,#ffffff,e5e5e5);
+
+	background-color:#ffffff;
+
+	border:1px solid #ffffff;
+	border-width:0px 1px 1px 0px;
+	text-align:left;
+	padding:10px;
+	font-size:15px;
+	font-family:Arial;
+	font-weight:bold;
+	color:#000000;
+}.TableID tr:last-child td{
+	border-width:0px 1px 0px 0px;
+}.TableID tr td:last-child{
+	border-width:0px 0px 1px 0px;
+}.TableID tr:last-child td:last-child{
+	border-width:0px 0px 0px 0px;
+}
+.TableID tr:first-child td{
+		background:-o-linear-gradient(bottom, #cccccc 5%, #b2b2b2 100%);	background:-webkit-gradient( linear, left top, left bottom, color-stop(0.05, #cccccc), color-stop(1, #b2b2b2) );
+	background:-moz-linear-gradient( center top, #cccccc 5%, #b2b2b2 100% );
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr="#cccccc", endColorstr="#b2b2b2");	background: -o-linear-gradient(top,#cccccc,b2b2b2);
+
+	background-color:#cccccc;
+	border:0px solid #ffffff;
+	text-align:center;
+	border-width:0px 0px 1px 1px;
+	font-size:20px;
+	font-family:Arial;
+	font-weight:bold;
+	color:#000000;
+}
+.TableID tr:first-child:hover td{
+	background:-o-linear-gradient(bottom, #cccccc 5%, #b2b2b2 100%);	background:-webkit-gradient( linear, left top, left bottom, color-stop(0.05, #cccccc), color-stop(1, #b2b2b2) );
+	background:-moz-linear-gradient( center top, #cccccc 5%, #b2b2b2 100% );
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr="#cccccc", endColorstr="#b2b2b2");	background: -o-linear-gradient(top,#cccccc,b2b2b2);
+
+	background-color:#cccccc;
+}
+.TableID tr:first-child td:first-child{
+	border-width:0px 0px 1px 0px;
+}
+.TableID tr:first-child td:last-child{
+	border-width:0px 0px 1px 1px;
+}
+
+
+
+.myButton {
+	-moz-box-shadow:inset 0px 1px 0px 0px #ffffff;
+	-webkit-box-shadow:inset 0px 1px 0px 0px #ffffff;
+	box-shadow:inset 0px 1px 0px 0px #ffffff;
+	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #ededed), color-stop(1, #dfdfdf));
+	background:-moz-linear-gradient(top, #ededed 5%, #dfdfdf 100%);
+	background:-webkit-linear-gradient(top, #ededed 5%, #dfdfdf 100%);
+	background:-o-linear-gradient(top, #ededed 5%, #dfdfdf 100%);
+	background:-ms-linear-gradient(top, #ededed 5%, #dfdfdf 100%);
+	background:linear-gradient(to bottom, #ededed 5%, #dfdfdf 100%);
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#ededed', endColorstr='#dfdfdf',GradientType=0);
+	background-color:#ededed;
+	-moz-border-radius:6px;
+	-webkit-border-radius:6px;
+	border-radius:6px;
+	border:1px solid #dcdcdc;
+	display:inline-block;
+	cursor:pointer;
+	color:#777777;
+	font-family:arial;
+	font-size:15px;
+	font-weight:bold;
+	padding:6px 24px;
+	text-decoration:none;
+	text-shadow:0px 1px 0px #ffffff;
+}
+.myButton:hover {
+	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #dfdfdf), color-stop(1, #ededed));
+	background:-moz-linear-gradient(top, #dfdfdf 5%, #ededed 100%);
+	background:-webkit-linear-gradient(top, #dfdfdf 5%, #ededed 100%);
+	background:-o-linear-gradient(top, #dfdfdf 5%, #ededed 100%);
+	background:-ms-linear-gradient(top, #dfdfdf 5%, #ededed 100%);
+	background:linear-gradient(to bottom, #dfdfdf 5%, #ededed 100%);
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#dfdfdf', endColorstr='#ededed',GradientType=0);
+	background-color:#dfdfdf;
+}
+.myButton:active {
+	position:relative;
+	top:1px;
+}
+
+
+.myButton1 {
+	-moz-box-shadow:inset 0px 1px 0px 0px #ffffff;
+	-webkit-box-shadow:inset 0px 1px 0px 0px #ffffff;
+	box-shadow:inset 0px 1px 0px 0px #ffffff;
+	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #f9f9f9), color-stop(1, #e9e9e9));
+	background:-moz-linear-gradient(top, #f9f9f9 5%, #e9e9e9 100%);
+	background:-webkit-linear-gradient(top, #f9f9f9 5%, #e9e9e9 100%);
+	background:-o-linear-gradient(top, #f9f9f9 5%, #e9e9e9 100%);
+	background:-ms-linear-gradient(top, #f9f9f9 5%, #e9e9e9 100%);
+	background:linear-gradient(to bottom, #f9f9f9 5%, #e9e9e9 100%);
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#f9f9f9', endColorstr='#e9e9e9',GradientType=0);
+	background-color:#f9f9f9;
+	-moz-border-radius:6px;
+	-webkit-border-radius:6px;
+	border-radius:6px;
+	border:2px solid #dcdcdc;
+	display:inline-block;
+	cursor:pointer;
+	color:#666666;
+	font-family:arial;
+	font-size:15px;
+	font-weight:bold;
+	padding:10px 48px;
+	text-decoration:none;
+	text-shadow:0px 1px 0px #ffffff;
+}
+.myButton1:hover {
+	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #e9e9e9), color-stop(1, #f9f9f9));
+	background:-moz-linear-gradient(top, #e9e9e9 5%, #f9f9f9 100%);
+	background:-webkit-linear-gradient(top, #e9e9e9 5%, #f9f9f9 100%);
+	background:-o-linear-gradient(top, #e9e9e9 5%, #f9f9f9 100%);
+	background:-ms-linear-gradient(top, #e9e9e9 5%, #f9f9f9 100%);
+	background:linear-gradient(to bottom, #e9e9e9 5%, #f9f9f9 100%);
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#e9e9e9', endColorstr='#f9f9f9',GradientType=0);
+	background-color:#e9e9e9;
+}
+.myButton1:active {
+	position:relative;
+	top:1px;
+}
+
+
+}
+</style>
+<center>
 	<title>Home</title>
 	<?php
 {
@@ -11,11 +201,12 @@
 function ViewClient() {
     $viewquery = mysql_query("SELECT * from tbl_client") or die(mysql_error());
 	echo '<p> View Client</p>
-	<table border="1" cellpadding="10">
+	<div class = 'TableID'>
+	<table border="1" cellpadding="10" TableID>
 	 <tr>
-	 <th>ClientName</th>
-	 <th>Client Contact Person</th> 
-	 <th>Client Address</th>
+	 <td>ClientName</td>
+	 <td>Client Contact Person</td> 
+	 <td>Client Address</td>
 	 </tr>';
 	  while($row = mysql_fetch_array( $viewquery )) {
 		echo '<tr>
@@ -23,21 +214,27 @@ function ViewClient() {
 		<td>' . $row['client_contactperson'] . '</td>
 		<td>' . $row['client_address'] . '</td>
 		<td>
+		<center>
 		<form method = "post" action="edit_client.php?client_id=' . $row['client_id'] . '">
-			<input type = "submit" name= "submitfromhome" value = "Edit">
+			<input type = "submit" name= "submitfromhome" value = "Edit" class = "myButton">
 		</form>
+		</center>
 		</td>
 		<td>
+		<center>
 		<form method = "post" action ="delete.php?client_id=' . $row['client_id'] . '">
-			<input type = "submit" name = "delsubmithome" value = "Delete">
+			<input type = "submit" name = "delsubmithome" value = "Delete" class = "myButton">
 		</form>
+		</center>
 		</td>
 		</tr>';
 	} 
       echo '</table>
+      </div>
+      
       <div>
       <form method = "post" action ="add_client.php">
-			<input type = "submit" name = "addsubmithome" value = "Add New Record">
+			<input type = "submit" name = "addsubmithome" value = "Add New Record" class= "myButton1">
 	  </form>
 	  </div>';
 
@@ -45,11 +242,12 @@ function ViewClient() {
 function ViewSupplier(){
 	$viewquery = mysql_query("SELECT * from tbl_supplier") or die(mysql_error());
 	echo '<p> View Supplier</p>
+	<div class = 'TableID'>
 	 <table border="1" cellpadding="10">
 	   <tr>
-	   <th>Supplier Name</th>
-	   <th>Supplier Contact Person</th>
-	   <th>Supplier Address</th>
+	   <td>Supplier Name</td>
+	   <td>Supplier Contact Person</td>
+	   <td>Supplier Address</td>
 	   </tr>';
 	  while($row = mysql_fetch_array( $viewquery )) {
 		
@@ -58,23 +256,30 @@ function ViewSupplier(){
 		<td>' . $row['supplier_contactperson'] . '</td>
 		<td>' . $row['supplier_address'] . '</td>
 		<td>
+		<center>
 		<form method = "post" action="edit_supplier.php?supplier_id=' . $row['supplier_id'] . '">
-			<input type = "submit" name= "submitfromhome" value = "Edit">
+			<input type = "submit" name= "submitfromhome" value = "Edit" class = "myButton">
 		</form>
+		</center>
 		</td>
 		<td>
+		<center>
 		<form method = "post" action ="delete.php?supplier_id=' . $row['supplier_id'] . '">
-			<input type = "submit" name = "delsubmithome" value = "Delete">
+			<input type = "submit" name = "delsubmithome" value = "Delete"  class = "myButton">
 		</form>
+		</center>
 		</td>
 		</tr>';
 	} 
       echo '</table>
+      </div>
+      
       <div>
       <form method = "post" action ="add_supplier.php">
-			<input type = "submit" name = "addsubmithome" value = "Add New Record">
+			<input type = "submit" name = "addsubmithome" value = "Add New Record" class= "myButton1">
 	  </form>
 	  </div>';
+</center>
 }
 ?>
 </head>
